b2d1fca38bd44ae77a133d121249ad53d450a5d2
0xromrom <roman0811@live.nl>
2022-04-04 05:17:28 -0700

    tweaked the mobile navbar, join us button is hidden upon open, and is also listed in the menu instead

--- js/main.js
+++ js/main.js
@@ -3,11 +3,30 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-links");
 const navbar = document.querySelector(".nav-section");
+const joinUsBtn = document.querySelector(".cta1");
 
 //Burger menu for mobile
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
+  joinUsBtn.classList.toggle("opacity");
+  navMenu.innerHTML = `<li class="nav-link">
+    <a href="#" class="underline">Home</a>
+  </li>
+  <li class="nav-link">
+    <a href="#" class="underline">Crypto News</a>
+  </li>
+  <li class="nav-link">
+    <a href="#" class="underline">Tutorials</a>
+  </li>
+  <li class="nav-link">
+    <a href="#" class="underline">Project Review</a>
+  </li><li class="nav-link">
+  <a href="#" class="underline">Quick Links</a>
+</li><li class="nav-link">
+<a href="#" class="underline">Join us</a>
+</li>
+  `;
 });
 
 //Navigation upon scroll transparent
