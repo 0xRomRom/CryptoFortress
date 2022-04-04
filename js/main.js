c08115aeab37be507da8e4273c8cfb59adcaa65b
0xromrom <roman0811@live.nl>
2022-04-03 20:11:26 -0700

    Added a few media queries and a hamburger drop menu

---
+++ js/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
