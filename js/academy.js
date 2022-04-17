a4b37ae06076cd15c3b33f2bd170bda73981513c
0xromrom <roman0811@live.nl>
2022-04-16 20:39:30 -0700

    Pre-content loadscreen nearly setup

--- js/academy.js
+++ js/academy.js
@@ -16,18 +16,21 @@ hamburger.addEventListener("click", () => {
   joinUsBtn.classList.toggle("opacity");
 });
 
+//Arcade machine functionality
 startButton.classList.remove("hide-link");
-startButton.addEventListener("click", () => {
-  launchButton.classList.remove("hide-link");
-  startButton.classList.add("hide-link");
-  displayValue.textContent = "";
-  launchButton.value = "Parsing";
-  displayDataLoading.classList.remove("hide-link");
 
+startButton.addEventListener("click", () => {
+  startButton.classList.add("hide-link");
+  launchButton.classList.remove("hide-link");
+  displayValue.textContent = "";
+  displayDataLoading.classList.remove("hide-link");
+  launchButton.value = "Parsing";
   setTimeout(function () {
     launchButton.value = "launch";
     launchButton.style.color = "white";
     launchButton.style.backgroundColor = "red";
     displayDataLoading.classList.add("hide-link");
+    displayValue.style.paddingLeft = "50px";
+    displayValue.textContent = "Ready to learn?";
   }, 4000);
 });
