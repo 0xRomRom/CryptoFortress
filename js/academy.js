91db77db59cf21baca941e7f7e7094f696930b29
0xromrom <roman0811@live.nl>
2022-04-16 17:52:26 -0700

    Added button functionality and inserted gif

--- js/academy.js
+++ js/academy.js
@@ -4,10 +4,23 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-links");
 const joinUsBtn = document.querySelector(".cta1");
+const startButton = document.querySelector(".start-button");
+const launchButton = document.querySelector(".launch-button");
+const displayValue = document.querySelector(".text-call1");
+const displayDataLoading = document.querySelector(".loading-data");
 
 //Burger menu for mobile
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
   joinUsBtn.classList.toggle("opacity");
+});
+
+startButton.classList.remove("hide-link");
+startButton.addEventListener("click", () => {
+  launchButton.classList.remove("hide-link");
+  startButton.classList.add("hide-link");
+  displayValue.textContent = "";
+  launchButton.value = "Parsing";
+  displayDataLoading.classList.remove("hide-link");
 });
